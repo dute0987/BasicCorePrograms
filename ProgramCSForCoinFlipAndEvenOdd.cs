7a096b4cbee1337b365272668d24eab1609ed92c
dute0987 <dute0987@gmail.com>
2022-02-23 18:53:47 +0530

    Greeting message for Basic Core Program

--- ProgramCSForCoinFlipAndEvenOdd.cs
+++ ProgramCSForCoinFlipAndEvenOdd.cs
@@ -5,10 +5,10 @@ namespace coinFlip
     {
         static void Main(string[]args)
         {
-	    //Console.WriteLine("Welcome To Basic Core Program");	
-              coinFlip.FlipTheCoin();
-            //SwitchDemo.DisplayMonth();
-            //findEvenOdd.EvenOdd();
+	     //Console.WriteLine("Welcome To Basic Core Program");	
+               coinFlip.FlipTheCoin();
+             //SwitchDemo.DisplayMonth();
+             //findEvenOdd.EvenOdd();
         }
     }
 }
