4ae8216a444573684d7978dfdcc4921d4e9771ae
dute0987 <dute0987@gmail.com>
2022-02-23 19:51:07 +0530

    Main file for finding largest number

---
+++ FindingLargest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograming
+{
+    public class FindingLargest
+    {
+        public static void FindTheLargeNumber()
+        {
+            Console.WriteLine("Find the largest number of three number");
+            int num1, num2, num3;
+
+            Console.WriteLine("Input the first number");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("input the third number");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1>num2 && num1>num3)
+            {
+                Console.WriteLine("num1 is greater then num2 and num3");
+            }
+            else if (num2> num1 && num2>num3)
+            {
+                Console.WriteLine("num2 is greater than num1 and num3");
+            }
+            else
+            {
+                Console.WriteLine("num3 is greater than num1 and num2");
+            }
+        }
+    }
+}
