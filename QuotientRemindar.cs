ca928ca1bce7adecd5895183121cc7554de3c3f2
dute0987 <dute0987@gmail.com>
2022-02-23 19:29:44 +0530

    Main Program of quotient and remaindar

---
+++ QuotientRemindar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograming
+{
+    public class QuotientRemindar
+    {
+        public static void FindQuotientAndRemindar()
+        {
+            int dividend = 50, divisor = 4;
+            int quotient = dividend / divisor;
+
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend : {0},Divisor : {1}", divisor, dividend);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remaindar = " + remainder);
+
+        }
+    }
+}
