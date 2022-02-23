6cd4ff35037b5049771435a02a9229d16622b808
dute0987 <dute0987@gmail.com>
2022-02-23 19:28:14 +0530

    Program.cs for Program of quotient and remaindar

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BasicCorePrograming
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            //VowelAndConstsnt.CheckVowelAndConstant();
+            //PrimeNumberProgram.FindPrimeNumber();
+            //SwapTheNumber.TwoNumberSwap();
+            //FindingLargest.FindTheLargeNumber();
+            QuotientRemindar.FindQuotientAndRemindar();
+        }
+    }
+}
