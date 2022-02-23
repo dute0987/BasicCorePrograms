c40ab7d817ceddb07ad2aa6a7ddc355d2e9aef44
dute0987 <dute0987@gmail.com>
2022-02-23 18:45:22 +0530

    Program.cs for Coinflip Program

--- ProgramCSForCoinFlipAndEvenOdd.cs
+++ ProgramCSForCoinFlipAndEvenOdd.cs
@@ -5,8 +5,8 @@ namespace coinFlip
     {
         static void Main(string[]args)
         {
-	     Console.WriteLine("Welcome To Basic Core Program");	
-            //coinFlip.FlipTheCoin();
+	    //Console.WriteLine("Welcome To Basic Core Program");	
+              coinFlip.FlipTheCoin();
             //SwitchDemo.DisplayMonth();
             //findEvenOdd.EvenOdd();
         }
